b5b78d259f2faac1ca3dda557850aeb0ce751337
Adrian Aviles <adrian8167e@gmail.com>
2023-12-13 17:49:21 -0700

    Update documentation
    Update XML Doc for dependency injection extension method.

--- Flowsy.Mediation/DependencyInjection.cs
+++ Flowsy.Mediation/DependencyInjection.cs
@@ -14,11 +14,12 @@ public static class DependencyInjection
         params Assembly[] assemblies
         )
         => services.AddMediation(null, assemblies);
-    
+
     /// <summary>
     /// Registers assemblies containing requests and request handlers to trigger application processes. 
     /// </summary>
     /// <param name="services">The application service collection.</param>
+    /// <param name="configure">An action con configure an MediatRServiceConfiguration instance.</param>
     /// <param name="assemblies">The assemblies to register requests and request handlers from.</param>
     /// <returns>The application service collection.</returns>
     public static MediationBuilder AddMediation(
